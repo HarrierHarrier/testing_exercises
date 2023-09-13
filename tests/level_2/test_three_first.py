2e1abf9e624f84e39efd922342ab8e803bd4816e
Vitaly Lunev <v.lunev.mail@gmail.com>
2023-09-14 01:52:22 +0300

    Task three

---
+++ tests/level_2/test_three_first.py
@@ -0,0 +1,21 @@
+import pytest
+
+from functions.level_2.three_first import first
+
+
+def test__first__get_first_element():
+    assert first([1, 2, 3]) == 1
+
+
+def test__first__get_custom_default():
+    assert first([], 1) == 1
+
+
+def test__first__raise_default_not_set():
+    with pytest.raises(AttributeError):
+        first([])
+
+
+def test__first__raise_wrong_type():
+    with pytest.raises(AttributeError):
+        first(None)
