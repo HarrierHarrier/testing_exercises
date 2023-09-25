c50d5312757f72abdc488fdf9cc18899aed3647e
Vitaly Lunev <v.lunev.mail@gmail.com>
2023-09-25 22:31:49 +0300

    Task four: changed literals type for non-empty sets

--- tests/level_2/test_four_sentiment.py
+++ tests/level_2/test_four_sentiment.py
@@ -3,17 +3,17 @@ from functions.level_2.four_sentiment import check_tweet_sentiment
 
 def test__check_tweet_sentiment__good_tweet():
     assert check_tweet_sentiment(
-        'Hello world', set(['world']), set()
+        'Hello world', {'world'}, set()
     ) == 'GOOD'
 
 
 def test__check_tweet_sentiment__bad_tweet():
     assert check_tweet_sentiment(
-        'Hello world', set(), set(['world'])
+        'Hello world', set(), {'world'}
     ) == 'BAD'
 
 
 def test__check_tweet_sentiment__neutral_tweet():
     assert check_tweet_sentiment(
-        'Hello world', set(['hello']), set(['world'])
+        'Hello world', {'hello'}, {'world'}
     ) is None
