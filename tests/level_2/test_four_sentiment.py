380a713a87d3ee521947b366823c9484e084a812
Vitaly Lunev <v.lunev.mail@gmail.com>
2023-09-25 22:44:03 +0300

    Task four: case sensitivity test functions

--- tests/level_2/test_four_sentiment.py
+++ tests/level_2/test_four_sentiment.py
@@ -17,3 +17,15 @@ def test__check_tweet_sentiment__neutral_tweet():
     assert check_tweet_sentiment(
         'Hello world', {'hello'}, {'world'}
     ) is None
+
+
+def test__check_tweet_sentiment__good_words_case_sensitivity():
+    assert check_tweet_sentiment(
+        'Hello world', {'Hello'}, set()
+    ) is None
+
+
+def test__check_tweet_sentiment__bad_words_case_sensitivity():
+    assert check_tweet_sentiment(
+        'Hello world', set(), {'World'}
+    ) is None
