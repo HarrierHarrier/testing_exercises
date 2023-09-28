7839ff3c4d365c6b926311c17a8bd81061130a64
Vitaly Lunev <v.lunev.mail@gmail.com>
2023-09-29 00:43:03 +0300

    Level 2: parametrize task four

--- tests/level_2/test_four_sentiment.py
+++ tests/level_2/test_four_sentiment.py
@@ -1,31 +1,28 @@
+import pytest
+
 from functions.level_2.four_sentiment import check_tweet_sentiment
 
 
-def test__check_tweet_sentiment__good_tweet():
+@pytest.mark.parametrize(
+    'text,good_words,bad_words,expected_result',
+    [
+        ('Hello world', {'world'}, set(), 'GOOD'),
+        ('Hello world', set(), {'world'}, 'BAD'),
+        ('Hello world', {'hello'}, {'world'}, None),
+        ('Hello world', {'Hello'}, set(), None),
+        ('Hello world', set(), {'World'}, None)
+    ],
+    ids=[
+        'good_tweet',
+        'bad_tweet',
+        'neutral_tweet',
+        'good_words_case_sensitivity',
+        'bad_words_case_sensitivity',
+    ]
+)
+def test__check_tweet_sentiment__success(
+    text, good_words, bad_words, expected_result
+):
     assert check_tweet_sentiment(
-        'Hello world', {'world'}, set()
-    ) == 'GOOD'
-
-
-def test__check_tweet_sentiment__bad_tweet():
-    assert check_tweet_sentiment(
-        'Hello world', set(), {'world'}
-    ) == 'BAD'
-
-
-def test__check_tweet_sentiment__neutral_tweet():
-    assert check_tweet_sentiment(
-        'Hello world', {'hello'}, {'world'}
-    ) is None
-
-
-def test__check_tweet_sentiment__good_words_case_sensitivity():
-    assert check_tweet_sentiment(
-        'Hello world', {'Hello'}, set()
-    ) is None
-
-
-def test__check_tweet_sentiment__bad_words_case_sensitivity():
-    assert check_tweet_sentiment(
-        'Hello world', set(), {'World'}
-    ) is None
+        text, good_words, bad_words
+    ) == expected_result
