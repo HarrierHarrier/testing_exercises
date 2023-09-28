21db3eb45c03a1c6c35d3f2eda695d9b2215c8b6
Vitaly Lunev <v.lunev.mail@gmail.com>
2023-09-28 23:47:41 +0300

    Level 1: parametrize task three

--- tests/level_1/test_three_url_builder.py
+++ tests/level_1/test_three_url_builder.py
@@ -1,16 +1,18 @@
+import pytest
+
 from functions.level_1.three_url_builder import build_url
 
 
-def test__build_url__check_default_params():
-    assert build_url(
-        host_name='https://github.com',
-        relative_url='HarrierHarrier/testing_exercises'
-    ) == 'https://github.com/HarrierHarrier/testing_exercises'
-
-
-def test__build_url__check_custom_params():
-    assert build_url(
-        host_name='https://github.com',
-        relative_url='HarrierHarrier/testing_exercises',
-        get_params={'a': 1, 'b': '2'}
-    ) == 'https://github.com/HarrierHarrier/testing_exercises?a=1&b=2'
+@pytest.mark.parametrize(
+    "host_name,relative_url,get_params,expected_result",
+    [
+        ('https://github.com', 'HarrierHarrier/testing_exercises', None, 'https://github.com/HarrierHarrier/testing_exercises'),
+        ('https://github.com', 'HarrierHarrier/testing_exercises', {'a': 1, 'b': '2'}, 'https://github.com/HarrierHarrier/testing_exercises?a=1&b=2'),
+    ],
+    ids=[
+        'default_params',
+        'custom_params',
+    ]
+)
+def test__build_url__success(host_name, relative_url, get_params, expected_result):
+    assert build_url(host_name, relative_url, get_params) == expected_result
