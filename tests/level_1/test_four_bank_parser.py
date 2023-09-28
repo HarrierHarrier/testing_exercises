54acea09d79c442cbf7fa2afb63490a7d940ec4d
Vitaly Lunev <v.lunev.mail@gmail.com>
2023-09-29 00:07:19 +0300

    Level 1: parametrize task four

--- tests/level_1/test_four_bank_parser.py
+++ tests/level_1/test_four_bank_parser.py
@@ -1,99 +1,131 @@
 import datetime
 import decimal
 
+import pytest
+
 from functions.level_1.four_bank_parser import (
     BankCard, SmsMessage, parse_ineco_expense
 )
 
 
-def test__parse_ineco_expense__amount_parsed_correctly():
-    card_a = BankCard(last_digits='1029', owner='Alice')
-    card_b = BankCard(last_digits='2910', owner='Bob')
-    sms = SmsMessage(
-        text='??? ??? 100.00 ???, 1029 01.01.23 04:20 Market authcode ****',
-        author='Green Bank',
-        sent_at=datetime.datetime(2023, 1, 2, 0, 0)
-    )
-
-    value = parse_ineco_expense(sms=sms, cards=[card_a, card_b])
-
-    assert value.amount == decimal.Decimal('100.00')
-
-
-def test__parse_ineco_expense__card_parsed_correctly():
-    card_a = BankCard(last_digits='1029', owner='Alice')
-    card_b = BankCard(last_digits='2910', owner='Bob')
-    sms = SmsMessage(
-        text='??? ??? 100.00 ???, 1029 01.01.23 04:20 Market authcode ****',
-        author='Green Bank',
-        sent_at=datetime.datetime(2023, 1, 2, 0, 0)
-    )
-
-    value = parse_ineco_expense(sms=sms, cards=[card_a, card_b])
-
-    assert value.card == card_a
+@pytest.mark.parametrize(
+    'sms,cards,expected_result',
+    [
+        (
+            SmsMessage(
+                text='??? ??? 100.00 ???, 1029 01.01.23 04:20 Market authcode ****',
+                author='Green Bank',
+                sent_at=datetime.datetime(2023, 1, 2, 0, 0)
+            ),
+            [
+                BankCard(last_digits='1029', owner='Alice'),
+                BankCard(last_digits='2910', owner='Bob')
+            ],
+            decimal.Decimal('100.00')
+        )
+    ]
+)
+def test__parse_ineco_expense__amount(sms, cards, expected_result):
+    assert parse_ineco_expense(sms, cards).amount == expected_result
 
 
-def test__parse_ineco_expense__spent_in_one_word_parsed_correctly():
-    card_a = BankCard(last_digits='1029', owner='Alice')
-    card_b = BankCard(last_digits='2910', owner='Bob')
-    sms = SmsMessage(
-        text=(
-            '??? ??? 100.00 ???, 1029 01.01.23 04:20 Market'
+@pytest.mark.parametrize(
+    'sms,cards,expected_result',
+    [
+        (
+            SmsMessage(
+                text='??? ??? 100.00 ???, 1029 01.01.23 04:20 Market authcode ****',
+                author='Green Bank',
+                sent_at=datetime.datetime(2023, 1, 2, 0, 0)
+            ),
+            [
+                BankCard(last_digits='1029', owner='Alice'),
+                BankCard(last_digits='2910', owner='Bob')
+            ],
+            BankCard(last_digits='1029', owner='Alice')
+        )
+    ]
+)
+def test__parse_ineco_expense__card(sms, cards, expected_result):
+    assert parse_ineco_expense(sms, cards).card == expected_result
+
+
+@pytest.mark.parametrize(
+    'sms,cards,expected_result',
+    [
+        (
+            SmsMessage(
+                text=(
+                    '??? ??? 100.00 ???, 1029 01.01.23 04:20 Market'
+                ),
+                author='Green Bank',
+                sent_at=datetime.datetime(2023, 1, 2, 0, 0)
+            ),
+            [
+                BankCard(last_digits='1029', owner='Alice'),
+                BankCard(last_digits='2910', owner='Bob')
+            ],
+            'Market'
         ),
-        author='Green Bank',
-        sent_at=datetime.datetime(2023, 1, 2, 0, 0)
-    )
-
-    value = parse_ineco_expense(sms=sms, cards=[card_a, card_b])
-
-    assert value.spent_in == 'Market'
-
-
-def test__parse_ineco_expense__spent_in_multiple_words_parsed_correctly():
-    card_a = BankCard(last_digits='1029', owner='Alice')
-    card_b = BankCard(last_digits='2910', owner='Bob')
-    sms = SmsMessage(
-        text=(
-            '??? ??? 100.00 ???, 1029 01.01.23 04:20 Local Market Nearby'
+        (
+            SmsMessage(
+                text=(
+                    '??? ??? 100.00 ???, 1029 01.01.23 04:20 Local Market Nearby'
+                ),
+                author='Green Bank',
+                sent_at=datetime.datetime(2023, 1, 2, 0, 0)
+            ),
+            [
+                BankCard(last_digits='1029', owner='Alice'),
+                BankCard(last_digits='2910', owner='Bob')
+            ],
+            'Local Market Nearby'
         ),
-        author='Green Bank',
-        sent_at=datetime.datetime(2023, 1, 2, 0, 0)
-    )
+        (
+            SmsMessage(
+                text=(
+                    '??? ??? 100.00 ???, 1029 01.01.23 04:20 Local Market Nearby'
+                    ' authcode ****'
+                ),
+                author='Green Bank',
+                sent_at=datetime.datetime(2023, 1, 2, 0, 0)
+            ),
+            [
+                BankCard(last_digits='1029', owner='Alice'),
+                BankCard(last_digits='2910', owner='Bob')
+            ],
+            'Local Market Nearby'
+        )
+    ],
+    ids=[
+        'one_word',
+        'multiple_words',
+        'with_authcode'
+    ]
+)
+def test__parse_ineco_expense__spent_in(sms, cards, expected_result):
+    assert parse_ineco_expense(sms, cards).spent_in == expected_result
 
-    value = parse_ineco_expense(sms=sms, cards=[card_a, card_b])
 
-    assert value.spent_in == 'Local Market Nearby'
-
-
-def test__parse_ineco_expense__spent_in_with_authcode_parsed_correctly():
-    card_a = BankCard(last_digits='1029', owner='Alice')
-    card_b = BankCard(last_digits='2910', owner='Bob')
-    sms = SmsMessage(
-        text=(
-            '??? ??? 100.00 ???, 1029 01.01.23 04:20 Local Market Nearby'
-            ' authcode ****'
-        ),
-        author='Green Bank',
-        sent_at=datetime.datetime(2023, 1, 2, 0, 0)
-    )
-
-    value = parse_ineco_expense(sms=sms, cards=[card_a, card_b])
-
-    assert value.spent_in == 'Local Market Nearby'
-
-
-def test__parse_ineco_expense__spent_at_parsed_correctly():
-    card_a = BankCard(last_digits='1029', owner='Alice')
-    card_b = BankCard(last_digits='2910', owner='Bob')
-    sms = SmsMessage(
-        text=(
-            '??? ??? 100.00 ???, 1029 01.01.23 04:20 Central Market '
-            'authcode ****'
-        ),
-        author='Green Bank',
-        sent_at=datetime.datetime(2023, 1, 2, 0, 0)
-    )
-    value = parse_ineco_expense(sms=sms, cards=[card_a, card_b])
-
-    assert value.spent_at == datetime.datetime(2023, 1, 1, 4, 20)
+@pytest.mark.parametrize(
+    'sms,cards,expected_result',
+    [
+        (
+            SmsMessage(
+                text=(
+                    '??? ??? 100.00 ???, 1029 01.01.23 04:20 Central Market '
+                    'authcode ****'
+                ),
+                author='Green Bank',
+                sent_at=datetime.datetime(2023, 1, 2, 0, 0)
+            ),
+            [
+                BankCard(last_digits='1029', owner='Alice'),
+                BankCard(last_digits='2910', owner='Bob')
+            ],
+            datetime.datetime(2023, 1, 1, 4, 20)
+        )
+    ]
+)
+def test__parse_ineco_expense__spent_at(sms, cards, expected_result):
+    assert parse_ineco_expense(sms, cards).spent_at == expected_result
