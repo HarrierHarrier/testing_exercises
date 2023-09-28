0d2104339373d559e8d9b01788151d897386dba5
Vitaly Lunev <v.lunev.mail@gmail.com>
2023-09-29 00:16:29 +0300

    Level 1: parametrize task five

--- tests/level_1/test_five_title.py
+++ tests/level_1/test_five_title.py
@@ -1,17 +1,22 @@
+import pytest
+
 from functions.level_1.five_title import change_copy_item
 
 
-def test__change_copy_item__add_prefix():
-    assert change_copy_item('Item') == 'Copy of Item'
-
-
-def test__change_copy_item__add_copy_number():
-    assert change_copy_item('Copy of Item') == 'Copy of Item (2)'
-
-
-def test__change_copy_item__increase_copy_number():
-    assert change_copy_item('Copy of Item (2)') == 'Copy of Item (3)'
-
-
-def test__change_copy_item__check_max_length():
-    assert change_copy_item('Item', 4) == 'Item'
+@pytest.mark.parametrize(
+    'title,max_main_item_title_length,expected_result',
+    [
+        ('Item', 100, 'Copy of Item'),
+        ('Copy of Item', 100, 'Copy of Item (2)'),
+        ('Copy of Item (2)', 100, 'Copy of Item (3)'),
+        ('Item', 4, 'Item'),
+    ],
+    ids=[
+        'add_prefix',
+        'add_copy_number',
+        'increase_copy_number',
+        'exceed_max_length',
+    ]
+)
+def test__change_copy_item__success(title, max_main_item_title_length, expected_result):
+    assert change_copy_item(title, max_main_item_title_length) == expected_result
