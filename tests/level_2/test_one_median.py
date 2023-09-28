40987256aa6538bff206db39730de500b5707aa7
Vitaly Lunev <v.lunev.mail@gmail.com>
2023-09-29 00:26:39 +0300

    Level 2: parametrize task one

--- tests/level_2/test_one_median.py
+++ tests/level_2/test_one_median.py
@@ -3,13 +3,21 @@ import pytest
 from functions.level_2.one_median import get_median_value
 
 
-def test__get_median_value__empty_list():
-    assert get_median_value([]) is None
-
-
-@pytest.mark.xfail(reason='wrong implementation')
-def test__get_median_value__odd_items_num():
-    assert get_median_value([11, 9, 3, 5, 5]) == 5
+@pytest.mark.parametrize(
+    "items,expected_result",
+    [
+        ([], None),
+        pytest.param([11, 9, 3, 5, 5], 5, marks=pytest.mark.xfail(reason='wrong implementation')),
+        pytest.param([1, 3, 5, 7], 4, marks=pytest.mark.xfail(reason='wrong implementation')),
+    ],
+    ids=[
+        'empty_list',
+        'odd_items_num',
+        'even_items_num'
+    ]
+)
+def test__get_median_value__success(items, expected_result):
+    assert get_median_value(items) == expected_result
 
 
 @pytest.mark.xfail(reason='wrong implementation')
@@ -18,8 +26,3 @@ def test__get_median_value__order_invariance():
     items_shuffled = [5, 11, 9, 3, 5]
 
     assert get_median_value(items) == get_median_value(items_shuffled)
-
-
-@pytest.mark.xfail(reason='wrong implementation')
-def test__get_median_value__even_items_num():
-    assert get_median_value([1, 3, 5, 7]) == 4
