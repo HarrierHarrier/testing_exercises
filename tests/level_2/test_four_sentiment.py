b24da0ea4c768997d05679169eb978dbd89f670a
Vitaly Lunev <v.lunev.mail@gmail.com>
2023-10-03 23:32:34 +0300

    Level 2: parametrize task four

--- tests/level_2/test_four_sentiment.py
+++ tests/level_2/test_four_sentiment.py
@@ -1,31 +1,66 @@
+import pytest
+
 from functions.level_2.four_sentiment import check_tweet_sentiment
 
 
-def test__check_tweet_sentiment__good_tweet():
+@pytest.mark.parametrize(
+    "text,good_words,bad_words",
+    [
+        ("Hello world", {"world"}, set()),
+        (
+            "Hello!, How do you do?, How d'ye do?, Good afternoon!,"
+            " How are you?, Good man!",
+            {"hello", "good"}, set()
+        )
+    ]
+)
+def test__check_tweet_sentiment__good_tweet(text, good_words, bad_words):
     assert check_tweet_sentiment(
-        'Hello world', {'world'}, set()
-    ) == 'GOOD'
+        text, good_words, bad_words
+    ) == "GOOD"
 
 
-def test__check_tweet_sentiment__bad_tweet():
+@pytest.mark.parametrize(
+    "text,good_words,bad_words",
+    [
+        ("Hello world", set(), {"world"}),
+        (
+            "Hello!, How do you do?, How d'ye do?, Good afternoon!,"
+            " How are you?, Good man!",
+            set(), {"hello", "good"}
+        )
+    ]
+)
+def test__check_tweet_sentiment__bad_tweet(text, good_words, bad_words):
     assert check_tweet_sentiment(
-        'Hello world', set(), {'world'}
-    ) == 'BAD'
+        text, good_words, bad_words
+    ) == "BAD"
 
 
-def test__check_tweet_sentiment__neutral_tweet():
+@pytest.mark.parametrize(
+    "text,good_words,bad_words",
+    [
+        ("Hello world", {"hello"}, {"world"}),
+        (
+            "Hello!, How do you do?, How d'ye do?, Good afternoon!,"
+            " How are you?, Good man!",
+            {"good"}, {"do?,"}
+        )
+    ]
+)
+def test__check_tweet_sentiment__neutral_tweet(text, good_words, bad_words):
     assert check_tweet_sentiment(
-        'Hello world', {'hello'}, {'world'}
+        text, good_words, bad_words
     ) is None
 
 
 def test__check_tweet_sentiment__good_words_case_sensitivity():
     assert check_tweet_sentiment(
-        'Hello world', {'Hello'}, set()
+        "Hello world", {"Hello"}, set()
     ) is None
 
 
 def test__check_tweet_sentiment__bad_words_case_sensitivity():
     assert check_tweet_sentiment(
-        'Hello world', set(), {'World'}
+        "Hello world", set(), {"World"}
     ) is None
