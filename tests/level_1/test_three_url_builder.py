fe2d473e695ae8a6265913917304b1c381dce992
Vitaly Lunev <v.lunev.mail@gmail.com>
2023-10-03 22:58:01 +0300

    Level 1: parametrize task three

--- tests/level_1/test_three_url_builder.py
+++ tests/level_1/test_three_url_builder.py
@@ -1,16 +1,29 @@
+import pytest
+
 from functions.level_1.three_url_builder import build_url
 
 
-def test__build_url__check_default_params():
-    assert build_url(
-        host_name='https://github.com',
-        relative_url='HarrierHarrier/testing_exercises'
-    ) == 'https://github.com/HarrierHarrier/testing_exercises'
+@pytest.mark.parametrize(
+    "host_name,relative_url,expected_result",
+    [
+        ("https://github.com", "HarrierHarrier/testing_exercises", "https://github.com/HarrierHarrier/testing_exercises"),
+        ("https://github.com", "", "https://github.com/"),
+    ]
+)
+def test__build_url__check_default_params(
+    host_name, relative_url, expected_result
+):
+    assert build_url(host_name, relative_url) == expected_result
 
 
-def test__build_url__check_custom_params():
-    assert build_url(
-        host_name='https://github.com',
-        relative_url='HarrierHarrier/testing_exercises',
-        get_params={'a': 1, 'b': '2'}
-    ) == 'https://github.com/HarrierHarrier/testing_exercises?a=1&b=2'
+@pytest.mark.parametrize(
+    "host_name,relative_url,get_params,expected_result",
+    [
+        ("https://github.com", "HarrierHarrier/testing_exercises", {"a": 1, "b": "2"}, "https://github.com/HarrierHarrier/testing_exercises?a=1&b=2"),
+        ("https://github.com", "", {"foo": "spam", "hello": "world"}, "https://github.com/?foo=spam&hello=world"),
+    ]
+)
+def test__build_url__check_custom_params(
+    host_name, relative_url, get_params, expected_result
+):
+    assert build_url(host_name, relative_url, get_params) == expected_result
