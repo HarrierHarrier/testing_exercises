0c2bbbbbcba96da5b05f5d88585e4bfbf7bebdd5
Vitaly Lunev <v.lunev.mail@gmail.com>
2023-10-03 22:51:16 +0300

    Level 1: parametrize task two

--- tests/level_1/test_two_date_parser.py
+++ tests/level_1/test_two_date_parser.py
@@ -6,29 +6,53 @@ import pytest
 from functions.level_1.two_date_parser import compose_datetime_from
 
 
-@freeze_time('2023-01-01')
-def test__compose_datetime_from__date_str_tomorow():
-    test_value = datetime.datetime.now() + datetime.timedelta(days=1)
+@freeze_time("2023-01-01")
+@pytest.mark.parametrize(
+    "time_str",
+    [
+        "00:00",
+        "04:20",
+        "12:36",
+    ]
+)
+def test__compose_datetime_from__date_str_tomorow(time_str):
+    time_value = time_str.split(":")
+    test_value = datetime.datetime.now() + datetime.timedelta(
+        days=1, hours=int(time_value[0]), minutes=int(time_value[1])
+    )
 
-    value = compose_datetime_from('tomorrow', '00:00')
+    value = compose_datetime_from("tomorrow", time_str)
 
     assert value == test_value
 
 
-@freeze_time('2023-01-01')
-def test__compose_datetime_from__date_str_not_tomorow():
-    test_value = datetime.datetime.now()
+@freeze_time("2023-01-01")
+@pytest.mark.parametrize(
+    "date_str,time_str",
+    [
+        ("not tomorrow", "00:00"),
+        ("something", "04:20"),
+        ("something else", "12:36"),
+    ]
+)
+def test__compose_datetime_from__date_str_not_tomorow(date_str, time_str):
+    time_value = time_str.split(":")
+    test_value = datetime.datetime.now() + datetime.timedelta(
+        hours=int(time_value[0]), minutes=int(time_value[1])
+    )
 
-    value = compose_datetime_from('not tomorrow', '00:00')
+    value = compose_datetime_from(date_str, time_str)
 
     assert value == test_value
 
 
-def test__compose_datetime_from__invalid_time_str_value():
+@pytest.mark.parametrize(
+    "time_str",
+    [
+        "0420",
+        "hello world",
+    ]
+)
+def test__compose_datetime_from__invalid_time_str_value(time_str):
     with pytest.raises(ValueError):
-        compose_datetime_from('not tomorrow', '0911')
-
-
-def test__compose_datetime_from__invalid_time_str_type():
-    with pytest.raises(AttributeError):
-        compose_datetime_from('not tomorrow', None)
+        compose_datetime_from("not tomorrow", time_str)
