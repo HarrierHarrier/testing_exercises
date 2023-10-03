fb076b98df55ec2d98423001bca8940b1487e149
Vitaly Lunev <v.lunev.mail@gmail.com>
2023-10-03 23:33:51 +0300

    Level 2: parametrize task five

--- tests/level_2/test_five_replace_word.py
+++ tests/level_2/test_five_replace_word.py
@@ -1,35 +1,63 @@
+import pytest
+
 from functions.level_2.five_replace_word import replace_word
 
 
-def test__replace_word__old_word_removed():
-    old_word = 'hello'
+@pytest.mark.parametrize(
+    "replace_from,replace_to",
+    [
+        ("hello", "goodbye"),
+        ("world", "goodbye"),
+    ]
+)
+def test__replace_word__old_word_removed(replace_from, replace_to):
+    replaced_text = replace_word("hello world", replace_from, replace_to)
 
-    replaced_text = replace_word('hello world', old_word, 'goodbye')
-
-    assert old_word not in replaced_text
-
-
-def test_replace_word__new_word_appeared():
-    new_word = 'goodbye'
-
-    replaced_text = replace_word('hello world', 'hello', new_word)
-
-    assert new_word in replaced_text
+    assert replace_from not in replaced_text
 
 
-def test__replace_word__one_appearence_replaced():
-    assert replace_word('hello world', 'hello', 'goodbye') == 'goodbye world'
+@pytest.mark.parametrize(
+    "replace_from,replace_to",
+    [
+        ("hello", "goodbye"),
+        ("world", "goodbye"),
+    ]
+)
+def test_replace_word__new_word_appeared(replace_from, replace_to):
+    replaced_text = replace_word("hello world", replace_from, replace_to)
+
+    assert replace_to in replaced_text
 
 
-def test__replace_word__multiple_appearence_replaced():
-    assert replace_word(
-        'hello world hello', 'hello', 'goodbye'
-    ) == 'goodbye world goodbye'
+@pytest.mark.parametrize(
+    "text,replace_from,replace_to,expected_result",
+    [
+        ("hello world", "hello", "goodbye", "goodbye world"),
+        ("hello world", "world", "goodbye", "hello goodbye"),
+    ]
+)
+def test__replace_word__one_appearence_replaced(
+    text, replace_from, replace_to, expected_result
+):
+    assert replace_word(text, replace_from, replace_to) == expected_result
+
+
+@pytest.mark.parametrize(
+    "text,replace_from,replace_to,expected_result",
+    [
+        ("hello world hello", "hello", "goodbye", "goodbye world goodbye"),
+        ("hello world world", "world", "goodbye", "hello goodbye goodbye"),
+    ]
+)
+def test__replace_word__multiple_appearence_replaced(
+    text, replace_from, replace_to, expected_result
+):
+    assert replace_word(text, replace_from, replace_to) == expected_result
 
 
 def test__replace_word__case_sensitive_replace_from():
-    assert replace_word('Hello world', 'hello', 'goodbye') == 'goodbye world'
+    assert replace_word("Hello world", "hello", "goodbye") == "goodbye world"
 
 
 def test__replace_word__case_sensitive_replace_to():
-    assert replace_word('hello world', 'hello', 'Goodbye') == 'Goodbye world'
+    assert replace_word("hello world", "hello", "Goodbye") == "Goodbye world"
