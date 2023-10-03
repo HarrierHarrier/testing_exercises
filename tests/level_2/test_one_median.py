a5c80862abcb95db92f3f59821cc98e40476661a
Vitaly Lunev <v.lunev.mail@gmail.com>
2023-10-03 23:31:22 +0300

    Level 2: parametrize task one

--- tests/level_2/test_one_median.py
+++ tests/level_2/test_one_median.py
@@ -7,19 +7,36 @@ def test__get_median_value__empty_list():
     assert get_median_value([]) is None
 
 
-@pytest.mark.xfail(reason='wrong implementation')
-def test__get_median_value__odd_items_num():
-    assert get_median_value([11, 9, 3, 5, 5]) == 5
+# Тут parametrize помогает избежать того, что по случайности тест проходится
+# как XPASS
+@pytest.mark.xfail(reason="wrong implementation")
+@pytest.mark.parametrize(
+    "items,expected_result",
+    [
+        ([11, 9, 3, 5, 5], 5),
+        ([1, 2, 3, 4, 5], 3),
+    ]
+)
+def test__get_median_value__odd_items_num(items, expected_result):
+    assert get_median_value(items) == expected_result
 
 
-@pytest.mark.xfail(reason='wrong implementation')
+@pytest.mark.xfail(reason="wrong implementation")
+@pytest.mark.parametrize(
+    "items,expected_result",
+    [
+        ([1, 3, 5, 7], 4),
+        ([1, 1, 2, 3], 1),
+    ]
+)
+@pytest.mark.xfail(reason="wrong implementation")
+def test__get_median_value__even_items_num(items, expected_result):
+    assert get_median_value(items) == expected_result
+
+
+@pytest.mark.xfail(reason="wrong implementation")
 def test__get_median_value__order_invariance():
     items = [11, 9, 3, 5, 5]
     items_shuffled = [5, 11, 9, 3, 5]
 
     assert get_median_value(items) == get_median_value(items_shuffled)
-
-
-@pytest.mark.xfail(reason='wrong implementation')
-def test__get_median_value__even_items_num():
-    assert get_median_value([1, 3, 5, 7]) == 4
