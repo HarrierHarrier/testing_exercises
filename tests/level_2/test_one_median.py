ac40c695ca092f7179cffa5978c4a8157528442d
Vitaly Lunev <v.lunev.mail@gmail.com>
2023-10-10 00:26:38 +0300

    Level 2: task 1 - only failing parametars marked as xfail

--- tests/level_2/test_one_median.py
+++ tests/level_2/test_one_median.py
@@ -7,14 +7,11 @@ def test__get_median_value__empty_list():
     assert get_median_value([]) is None
 
 
-# Тут parametrize помогает избежать того, что по случайности тест проходится
-# как XPASS
-@pytest.mark.xfail(reason="wrong implementation")
 @pytest.mark.parametrize(
     ("items", "expected_result"),
     [
         ([11, 9, 3, 5, 5], 5),
-        ([1, 2, 3, 4, 5], 3),
+        pytest.param([1, 2, 3, 4, 5], 3, marks=pytest.mark.xfail(reason="wrong implementation")),
     ]
 )
 def test__get_median_value__odd_items_num(items, expected_result):
@@ -29,7 +26,6 @@ def test__get_median_value__odd_items_num(items, expected_result):
         ([1, 1, 2, 3], 1),
     ]
 )
-@pytest.mark.xfail(reason="wrong implementation")
 def test__get_median_value__even_items_num(items, expected_result):
     assert get_median_value(items) == expected_result
 
