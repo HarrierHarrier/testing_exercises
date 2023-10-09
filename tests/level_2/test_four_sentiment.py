0b4bdbe94157cad454e60425b590b7bde72d7554
Vitaly Lunev <v.lunev.mail@gmail.com>
2023-10-10 00:24:34 +0300

    Level 2: task 4 - unused parameters deleted & clearer examples used

--- tests/level_2/test_four_sentiment.py
+++ tests/level_2/test_four_sentiment.py
@@ -4,51 +4,63 @@ from functions.level_2.four_sentiment import check_tweet_sentiment
 
 
 @pytest.mark.parametrize(
-    "text,good_words,bad_words",
+    "text",
     [
-        ("Hello world", {"world"}, set()),
-        (
-            "Hello!, How do you do?, How d'ye do?, Good afternoon!,"
-            " How are you?, Good man!",
-            {"hello", "good"}, set()
-        )
+        "Good day",
+        "Excellent idea",
+        "This is awesome"
     ]
 )
-def test__check_tweet_sentiment__good_tweet(text, good_words, bad_words):
+def test__check_tweet_sentiment__good_tweet(text):
+    good_words = {
+        "good", "excellent", "awesome"
+    }
+    bad_words = {
+        "bad", "repulsive", "terrible"
+    }
+
     assert check_tweet_sentiment(
         text, good_words, bad_words
     ) == "GOOD"
 
 
 @pytest.mark.parametrize(
-    "text,good_words,bad_words",
+    "text",
     [
-        ("Hello world", set(), {"world"}),
-        (
-            "Hello!, How do you do?, How d'ye do?, Good afternoon!,"
-            " How are you?, Good man!",
-            set(), {"hello", "good"}
-        )
+        "I have a bad feeling about this",
+        "I find this repulsive",
+        "What a terrible idea"
     ]
 )
-def test__check_tweet_sentiment__bad_tweet(text, good_words, bad_words):
+def test__check_tweet_sentiment__bad_tweet(text):
+    good_words = {
+        "good", "excellent", "awesome"
+    }
+    bad_words = {
+        "bad", "repulsive", "terrible"
+    }
+
     assert check_tweet_sentiment(
         text, good_words, bad_words
     ) == "BAD"
 
 
 @pytest.mark.parametrize(
-    "text,good_words,bad_words",
+    "text",
     [
-        ("Hello world", {"hello"}, {"world"}),
-        (
-            "Hello!, How do you do?, How d'ye do?, Good afternoon!,"
-            " How are you?, Good man!",
-            {"good"}, {"do?,"}
-        )
+        "I don`t think it is bad , it`s good",
+        "You think this is excellent , but I find this repulsive",
+        "Ivan the Terrible had an awesome hat"
     ]
 )
-def test__check_tweet_sentiment__neutral_tweet(text, good_words, bad_words):
+def test__check_tweet_sentiment__neutral_tweet(text):
+    good_words = {
+        "good", "excellent", "awesome"
+    }
+    bad_words = {
+        "bad", "repulsive", "terrible"
+    }
+
     assert check_tweet_sentiment(
         text, good_words, bad_words
     ) is None
